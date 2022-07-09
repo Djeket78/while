820f3ed230ea4fba1df93977c490ca3b328ebcc7
Evghenii R <108763119+Djeket78@users.noreply.github.com>
2022-07-09 17:42:28 +0300

    work done//not if-eise

---
+++ 9,07while.py
@@ -0,0 +1,27 @@
+#count from 1 to 10 (while) 
+#start=1
+end=10
+step=1
+#############
+n=1
+while n<=end:
+    print(n)
+    n+=step
+##
+print("  #2#  ")
+##
+#start_m=5
+end_m=10
+m=5
+while m<=end_m:
+    print(m)
+    m+=step
+
+print("  #3#  ")
+##
+#start_k=10
+end_k=5
+k=10
+while k>=end_k:
+    print(k)
+    k-=step
